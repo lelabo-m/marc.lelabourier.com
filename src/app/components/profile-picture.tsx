2794796108d79cf88dbbc2bcf0b7a6b5882a901b
lelabo-m <marc.lelabourier@gmail.com>
2025-01-21 19:37:55 +0100

    Add a profile picture component

---
+++ src/app/components/profile-picture.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import profilePicture from "~/../public/profile.jpg";
+
+function ProfilePicture() {
+  return (
+    <div className="flex size-52 items-center justify-center">
+      <Image
+        className="size-full rounded-full object-cover"
+        src={profilePicture}
+        alt="Profile picture of Marc Le Labourier"
+      />
+    </div>
+  );
+}
+
+export default ProfilePicture;
