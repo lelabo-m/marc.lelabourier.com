85845a3ca6966f258f228971160731ea8043cda6
lelabo-m <marc.lelabourier@gmail.com>
2025-03-31 19:04:21 +0200

    Fix missing configuration when adding an env variable.

--- src/env.js
+++ src/env.js
@@ -26,6 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    DOMAIN: process.env.DOMAIN,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
