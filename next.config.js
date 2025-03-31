aeb172f421d5b74f747fff163ebdfc6af73bcae9
lelabo-m <marc.lelabourier@gmail.com>
2025-03-31 18:10:59 +0200

    Still trying to setup plausible.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
  * for Docker builds.
  */
 import createNextIntlPlugin from "next-intl/plugin";
+import { withPlausibleProxy } from "next-plausible";
 import "./src/env.js";
 
 const withNextIntl = createNextIntlPlugin("./src/app/_lib/i18n/request.ts");
@@ -10,4 +11,4 @@ const withNextIntl = createNextIntlPlugin("./src/app/_lib/i18n/request.ts");
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default withNextIntl(config);
+export default withPlausibleProxy()(withNextIntl(config));
