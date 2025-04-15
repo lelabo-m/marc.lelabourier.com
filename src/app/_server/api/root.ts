b6f77e897197db58f8822642fca661241a384f37
lelabo-m <marc.lelabourier@gmail.com>
2025-04-15 17:59:30 +0200

    Cleaning up

--- src/app/_server/api/root.ts
+++ src/app/_server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { resumeRouter } from "./routers/resume";
 
@@ -8,7 +7,6 @@ import { resumeRouter } from "./routers/resume";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   resume: resumeRouter,
 });
 
