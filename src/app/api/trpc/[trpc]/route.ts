b38de53253a98423759c2ecfea6e77c370064919
lelabo-m <marc.lelabourier@gmail.com>
2025-05-13 13:36:41 +0200

    Fixing server function timedout.

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -5,6 +5,8 @@ import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 import { env } from "~/env";
 
+export const maxDuration = 60;
+
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
